19808b5e7c12a05c2a3dd8231f085c823e4bf406
cudjoeab <cudjoeab@gmail.com>
2019-07-16 19:11:29 -0400

    added constructor, add, and plus tax instance methods

--- multi-class_shoppingcart/shopping_cart.py
+++ multi-class_shoppingcart/shopping_cart.py
@@ -4,6 +4,11 @@ class Shopping_cart:
     
     def add(self, product):
         self.cart.append(product) 
+
+    def plus_tax(self, base_price, tax_rate):
+        taxed_item = product.base_price * product.tax_rate
+        return taxed_item 
     
 
+    
 
