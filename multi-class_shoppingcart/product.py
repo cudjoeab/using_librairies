7b4874d8b09a44f10dd39f0baa2c5ad07262444b
cudjoeab <cudjoeab@gmail.com>
2019-07-16 21:38:57 -0400

    added tax instance functions to product class

--- multi-class_shoppingcart/product.py
+++ multi-class_shoppingcart/product.py
@@ -8,8 +8,19 @@ class Product:
         self.base_price = base_price
         self.tax_rate = tax_rate
 
+    def calculate_tax(self, base_price, tax_rate):
+        tax =  self.base_price * self.tax_rate   
+        return tax 
+    
+    def price_after_tax(self): 
+        price = self.base_price + self.tax
+        return price 
+    
+
 apples = Product('apples', 1, 0.08)
 chips = Product('chips', 4, 0.08)
 cheese = Product('cheese', 6, 0.08)
 salsa = Product('salsa', 5, 0.08)
 soda = Product('soda', 3, 0.08)
+
+
--- multi-class_shoppingcart/shopping_cart.py
+++ multi-class_shoppingcart/shopping_cart.py
@@ -1,13 +1,12 @@
 class Shopping_cart:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.cart = []
     
     def add(self, product):
         self.cart.append(product) 
 
-    def plus_tax(self, base_price, tax_rate):
-        taxed_item = product.base_price * product.tax_rate
-        return taxed_item 
+    
     
 
     
