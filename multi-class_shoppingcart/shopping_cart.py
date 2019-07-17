6ad25bf13cbf355c2ba124f6bcdd0eee44ea34e9
cudjoeab <cudjoeab@gmail.com>
2019-07-16 21:48:31 -0400

    add to cart, remove from cart, subtotal, and total tax instance methods

--- multi-class_shoppingcart/shopping_cart.py
+++ multi-class_shoppingcart/shopping_cart.py
@@ -3,10 +3,25 @@ class Shopping_cart:
         self.name = name
         self.cart = []
     
-    def add(self, product):
+    def add_to_cart(self, product):
         self.cart.append(product) 
+        return self.cart
 
+    def remove_from_cart(self, product):
+        self.cart.pop(product)
+        return self.cart 
     
+    def subtotal(self, cart):
+        for product in cart:
+            subtoal_sum += self.base_price 
+        return subtoal_sum 
+    
+    def total_tax(self, cart):
+        for product in cart:
+            total += self.total_tax
+        return total
+        
+        
     
 
     
