633b33473931a15b33e05976073b9bce86630389
cudjoeab <cudjoeab@gmail.com>
2019-07-16 22:14:21 -0400

    made changes in shopping cart instance methods

--- multi-class_shoppingcart/shopping_cart.py
+++ multi-class_shoppingcart/shopping_cart.py
@@ -5,27 +5,28 @@ class Shopping_cart:
         self.name = name
         self.cart = []
     
+    def __str__(self):
+        return f"{self.name}'s cart: {self.cart}"
+    
     def add_to_cart(self, product):
         self.cart.append(product) 
-        return self.cart
+        return f'{product.name} added to cart. Your cart: {self.cart}.'
 
     def remove_from_cart(self, product):
         self.cart.pop(product)
-        return self.cart 
+        return f'{product.name} removed from cart. Your cart: {self.cart}.' 
     
-    def subtotal(self, cart):
-        for product in cart:
-            subtoal_sum += self.base_price 
-        return subtoal_sum 
+    def total_before_tax(self):
+        subtotal = 0
+        for product in self.cart:
+            subtotal += product.base_price 
+        return subtotal
     
-    def total_tax(self, cart):
-        for product in cart:
-            total_tax_sum += self.total_tax
-        return total_tax_sum
-    
-    def total(self, cart):
-        total = subtotal_sum + total_tax_sum 
-        return 
+    def total_after_tax(self):
+        total = 0
+        for product in self.cart:
+            total += product.price_after_tax
+        return total 
 
         
         
